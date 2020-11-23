abb080fdc70fbbb9af3d9c845457d1e57aa96088
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-23 12:30:57 -0300

    2 columns

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -5,15 +5,21 @@ void main() => runApp(
         children: <Widget>[
           Text('Deliver features faster', textDirection: TextDirection.ltr,),
           Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
+          Text('Testando outro text', textDirection: TextDirection.ltr,),
+
           Expanded(
-            child: FittedBox(
-            fit: BoxFit.contain, // otherwise the logo will be tiny
-            child: const FlutterLogo(),
+            child:	FittedBox(
+              fit:	BoxFit.contain,	//	otherwise	the	logo	will	be	tiny
+              child:	const	FlutterLogo(),
+            ),
           ),
+      Column(
+            children : <Widget>[
+             Text('Outro Texto', textDirection: TextDirection.ltr,),
 
-         ),
 
-        ],
-
-      ),
+    ],
+   ),
+  ],
+ ),
 );
