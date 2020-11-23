24a4a903a2ebcc8a6129179f71036a6a39a7a3be
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-23 12:51:01 -0300

    Scaffold, Appbar, Icons, Column

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,25 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text('Deliver features faster', textDirection: TextDirection.ltr,),
-          Text('Craft beautiful UIs', textDirection: TextDirection.ltr,),
-          Text('Testando outro text', textDirection: TextDirection.ltr,),
-
-          Expanded(
-            child:	FittedBox(
-              fit:	BoxFit.contain,	//	otherwise	the	logo	will	be	tiny
-              child:	const	FlutterLogo(),
-            ),
-          ),
-      Column(
-            children : <Widget>[
-             Text('Outro Texto', textDirection: TextDirection.ltr,),
-
-
-    ],
-   ),
-  ],
- ),
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferencias'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          Text('Corpo do Scaffold'),
+        ],
+      ),
+    ),
+  )
 );
