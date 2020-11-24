56ab2e254e38cf9aa4821422a0cc3f18ed0e1971
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-24 10:06:16 -0300

    varios cards com column

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -9,13 +9,31 @@ void main() => runApp(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
-      body: Card(
-        child: ListTile(
-          leading: Icon(Icons.monetization_on),
-          title: Text('1500.0'),
-          subtitle: Text('Conta: 123-4'),
-        )
-       ),
+      body: Column(
+        children: [
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('1500.0'),
+              subtitle: Text('Conta: 123-4'),
+            )
+           ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('2500.0'),
+              subtitle: Text('Conta: 2334-4'),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('3500.0'),
+              subtitle: Text('Conta: 333-4'),
+            ),
+          )
+        ],
+      ),
     ),
   )
 );
