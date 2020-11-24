5aab85c9d09abea6d8c0eb2406b35235679961d3
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-24 10:25:57 -0300

    criando os cards atraves de classes

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,39 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Transferencias'),
+      MaterialApp(
+          home: Scaffold(
+        appBar: AppBar(
+          title: Text('Transferencias'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
+        body: Column(
+          children: [
+            iTemDeTransferencia('1500.0', '456-9'),
+            iTemDeTransferencia('2500.0', '3545-0'),
+            iTemDeTransferencia('1200.0', '7852-0'),
+          ],
+        ),
+      )),
+    );
+
+class iTemDeTransferencia extends StatelessWidget {
+  String valor;
+  String conta;
+
+  iTemDeTransferencia(this.valor, this.conta);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text('3500.0'),
+        subtitle: Text('Conta: 333-4'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-      body: Column(
-        children: [
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('1500.0'),
-              subtitle: Text('Conta: 123-4'),
-            )
-           ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('2500.0'),
-              subtitle: Text('Conta: 2334-4'),
-            ),
-          ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text('3500.0'),
-              subtitle: Text('Conta: 333-4'),
-            ),
-          )
-        ],
-      ),
-    ),
-  )
-);
+    );
+  }
+}
