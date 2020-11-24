3fbcdd0d4c241ae184885d9d8fb17b85e7cf5930
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-24 10:35:46 -0300

    listaDeTransferencia StateLessWidget

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() =>
+    runApp(
       MaterialApp(
           home: Scaffold(
-        appBar: AppBar(
-          title: Text('Transferencias'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-        body: Column(
-          children: [
-            iTemDeTransferencia('1500.0', '456-9'),
-            iTemDeTransferencia('2500.0', '3545-0'),
-            iTemDeTransferencia('1200.0', '7852-0'),
-          ],
-        ),
-      )),
+              appBar: AppBar(
+                title: Text('Transferencias'),
+              ),
+              floatingActionButton: FloatingActionButton(
+                child: Icon(Icons.add),
+              ),
+              body: listaDeTransferencia()
+          ),
+),
     );
 
 class iTemDeTransferencia extends StatelessWidget {
@@ -33,6 +29,19 @@ class iTemDeTransferencia extends StatelessWidget {
         title: Text('3500.0'),
         subtitle: Text('Conta: 333-4'),
       ),
+    );
+  }
+}
+
+class listaDeTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        iTemDeTransferencia('1500.0', '456-9'),
+        iTemDeTransferencia('2500.0', '3545-0'),
+        iTemDeTransferencia('1200.0', '7852-0'),
+      ],
     );
   }
 }
