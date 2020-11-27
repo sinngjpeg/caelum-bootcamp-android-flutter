85191812bdd29b2b065f603fbd69aa25edb6c87d
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-26 22:24:37 -0300

    Card com Column

--- bitbank/lib/main.dart
+++ bitbank/lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
         home: Scaffold(
+      body: Card(
+        child: Column(
+          children: <Widget>[
+            Text('?????????????/'),
+            Text('?????????????/'),
+            Text('?????????????/'),
+            Text('?????????????/'),
+            Text('?????????????/'),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('Transferencia'),
       ),
-
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-          ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     )));
