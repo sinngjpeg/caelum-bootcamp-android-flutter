0e069a31e3c2d3e8bacf554c7fb000d1564c69e6
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-27 00:22:38 -0300

    criando variavel valor,numeroDaConta, consntrutor

--- bitbank/lib/main.dart
+++ bitbank/lib/main.dart
@@ -16,20 +16,28 @@ class ListaTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        item_tranferencia(),
+        item_tranferencia('1000', '1020'),
+        item_tranferencia('2000', '1020'),
+        item_tranferencia('3000', '1030'),
+        item_tranferencia('4000', '1040'),
       ],
     );
   }
 }
 
 class item_tranferencia extends StatelessWidget {
+  final String valor;
+  final String numeroConta;
+
+  item_tranferencia(this.valor, this.numeroConta);
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
-        title: Text('200.0'),
-        subtitle: Text('2000'),
+        title: Text(valor),
+        subtitle: Text(numeroConta),
       ),
     );
   }
