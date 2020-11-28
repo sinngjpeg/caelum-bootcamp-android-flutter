12e12040011e919678991321a0b26f13b42bdf3b
Ingrid Silva <ingridsilvatec@outlook.com>
2020-11-28 19:21:04 -0300

    construtor com parametros opcinais {}

--- bitbank/lib/main.dart
+++ bitbank/lib/main.dart
@@ -27,8 +27,16 @@ class FormularioTransferencia extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          Editor(_controladorCampoNumeroConta, 'Numero da Conta', '0000'),
-          Editor((_controladorCampoValor), 'Valor', '0000'),
+          Editor(
+            controlador: _controladorCampoNumeroConta,
+            rotulo: 'Número da conta',
+            dica: '0000',
+          ),
+          Editor(
+              controlador: _controladorCampoValor,
+              rotulo: 'Valor',
+              dica: '0.00',
+              icone: Icons.monetization_on),
           RaisedButton(
               child: Text('Confirmar'),
               onPressed: () {
@@ -49,20 +57,22 @@ class FormularioTransferencia extends StatelessWidget {
 }
 
 class Editor extends StatelessWidget {
-  final TextEditingController _controlador;
-  final String _rotulo;
-  final String _dica;
+  final TextEditingController controlador;
+  final String rotulo;
+  final String dica;
+  final IconData icone;
 
-  Editor(this._controlador, this._rotulo, this._dica);
+  Editor({this.controlador, this.rotulo, this.dica, this.icone});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(16.0),
         child: TextField(
-          controller: _controlador,
+          controller: controlador,
           style: TextStyle(fontSize: 24.0),
-          decoration: InputDecoration(labelText: _rotulo, hintText: _dica),
+          decoration: InputDecoration(
+              icon: Icon(icone), labelText: rotulo, hintText: dica),
           keyboardType: TextInputType.number,
         ));
   }
