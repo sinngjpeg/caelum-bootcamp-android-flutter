6f9f19dfa30a29b2a0168f13625725a4b3c50a9d
Ingrid Silva <ingridsilvatec@outlook.com>
2020-12-01 22:58:03 -0300

    criando o padding para definir padroes do layout

--- mercado/lib/main.dart
+++ mercado/lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   final TextEditingController _controladorNome = TextEditingController();
   final TextEditingController _controladorQuantidade = TextEditingController();
   final TextEditingController _controladorValor = TextEditingController();
@@ -18,23 +17,45 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Cadastrando produto'),
         ),
-        body: Column(
-          children: <Widget>[
-            TextField(),
-            TextField(),
-            TextField(),
-            RaisedButton(
-              child: Text('Cadastrar'),
-              onPressed: () {
-                final String nome = _controladorNome.text;
-                final int quantidade = int.tryParse(_controladorQuantidade.text);
-                final double valor = double.tryParse(_controladorValor.text);
-
-                final Produto produtoNovo = Produto(nome, quantidade,valor);
-                print(produtoNovo);
-              },
-            )
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                controller: _controladorNome,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  controller: _controladorQuantidade,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                  controller: _controladorValor,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: RaisedButton(
+                  child: Text('Cadastrar'),
+                  onPressed: () {
+                    final String nome = _controladorNome.text;
+                    final int quantidade =
+                        int.tryParse(_controladorQuantidade.text);
+                    final double valor =
+                        double.tryParse(_controladorValor.text);
+                    if (nome != null && quantidade != null && valor != null) {
+                      final Produto produtoNovo =
+                          Produto(nome, quantidade, valor);
+                      print(produtoNovo);
+                    }
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
