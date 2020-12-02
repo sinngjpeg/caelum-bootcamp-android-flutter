e54e0c5a72714267226edbfbbddac852a63bdb54
Ingrid Silva <ingridsilvatec@outlook.com>
2020-12-01 21:51:17 -0300

    criando a classe Prodruto

--- mercado/lib/main.dart
+++ mercado/lib/main.dart
@@ -8,23 +8,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Cadastrando produto'),
-       ),
-       body: Column (
-         children: <Widget> [
-           TextField(),
-           TextField(),
-           TextField(),
-           RaisedButton(
-               child: Text('Cadastrar'),
-               onPressed: () {},
-           )
-         ],
-       ),
-     ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Cadastrando produto'),
+        ),
+        body: Column(
+          children: <Widget>[
+            TextField(),
+            TextField(),
+            TextField(),
+            RaisedButton(
+              child: Text('Cadastrar'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
+}
 
+class Produto {
+  final String nome;
+  final int quantidade;
+  final double valor;
+
+  Produto(
+    this.nome,
+    this.quantidade,
+    this.valor,
+  );
 }
